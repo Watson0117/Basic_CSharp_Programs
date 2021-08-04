0d0c72751712428bdbf5afa45a7cd56b146ded61
Joshua Watson <watson0117@gmail.com>
2021-08-04 12:11:57 -0600

    Acme Accounting Update
    use of  \n and  \"

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            // the \n makes the string after appear on the next line. the \" is use to create " withing the string that the compiler dosent use.
+            Console.WriteLine(" Welcom to Acme Accounting Systems \n Remember we're \"Accounting\" on you!"); // This will ask the user there name. they will enter in below in the string = name.
+            Console.Read(); // without this line the program will run and then be set so the next key press closes the cmd window.
+        }
+    }
+}
+
