7c01f297ebf9438f016fe474a074d4f1ec1e3fc5
Joshua Watson <watson0117@gmail.com>
2021-08-06 14:29:37 -0600

    example update

---
+++ CSChallengeWorkbook/CSChallengeWorkbook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSChallengeWorkbook
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = 3;
+            int num2 = 9;
+
+            int addTotal = num1 + num2;
+            Console.WriteLine(addTotal);
+
+            int subTotal = num2 - num1;
+            Console.WriteLine(subTotal);
+
+            int divTotal = num2 / num1;
+            Console.WriteLine(divTotal);
+
+            int mTotal = num1 * num2;
+            Console.WriteLine(mTotal);
+
+            int num3 = 200 / 3;
+            Console.WriteLine(num3);
+        }
+    }
+}
