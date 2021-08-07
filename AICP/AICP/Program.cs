e9636f237b78d538259cbb4adbcb8027b721d7d2
Joshua Watson <watson0117@gmail.com>
2021-08-07 15:17:19 -0600

    AICP Final
    Anonymous Income Comparison Program exercise

---
+++ AICP/AICP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AICP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program"); //Prints text
+            Console.WriteLine("Person1"); //Prints text
+            Console.WriteLine("Input Hourly Rate: "); //Prints text
+            string HR1 = Console.ReadLine(); // This gets input form the user
+            Console.WriteLine("Input Hours worked per week: "); //Prints text
+            string HPW1 = Console.ReadLine();// This gets input form the user
+            int HR2 = Convert.ToInt32(HR1); // convert to integer
+            int HPW2 = Convert.ToInt32(HPW1); // convert to integer
+
+            Console.WriteLine("Person2"); //Prints text
+            Console.WriteLine("Input Hourly Rate: "); //Prints text
+            string HR3 = Console.ReadLine();// This gets input form the user
+            Console.WriteLine("Input Hours worked per week: "); //Prints text
+            string HPW3 = Console.ReadLine();// This gets input form the user
+            int HR4 = Convert.ToInt32(HR3); // convert to integer
+            int HPW4 = Convert.ToInt32(HPW3); // convert to integer
+
+            int BS1 = HR2 * HPW2;
+            Console.WriteLine("Annual salary of Person 1:"); //Prints text
+            int ASP1 = BS1 * 52;
+            Console.WriteLine(ASP1); //Prints anual salary
+
+            int BS2 = HR4 * HPW4; // creates a weekly total
+            Console.WriteLine("Annual salary of Person 2:"); //Prints text
+            int ASP2 = BS2 * 52; // creates a weekly total
+            Console.WriteLine(ASP2); //Prints anual salary
+            Console.WriteLine("Does Person 1 make more money than Person 2 ?");// prints text
+            bool trueOrFalse = ASP1 > ASP2; // compare the annual salarys for eche person and returns T or F
+            Console.Write(trueOrFalse.ToString());//then prints 
+            Console.Read();// keeps the console from closing auto.
+
+        }
+    }
+}
--- CSChallengeWorkbook/CSChallengeWorkbook/Program.cs
+++ CSChallengeWorkbook/CSChallengeWorkbook/Program.cs
@@ -7,51 +7,48 @@ namespace CSChallengeWorkbook
         static void Main()
         {
 
-
-           
-
-
-            //string innum1;
-            //int innum150;
-            //Console.WriteLine("multiply by 50: ");
-            //innum1 = Console.ReadLine();
-            //innum150 = Convert.ToInt32(innum1) * 50; // convert to integer
-            //Console.WriteLine("Input multiplied by 50 =");
-            //Console.WriteLine(innum150); // display the line
+            string innum1;
+            int innum150;
+            Console.WriteLine("multiply by 50: ");
+            innum1 = Console.ReadLine();
+            innum150 = Convert.ToInt32(innum1) * 50; // convert to integer
+            Console.WriteLine("Input multiplied by 50 =");
+            Console.WriteLine(innum150); // display the line
 
 
-            //string innum2;
-            //int innum225;
-            //Console.WriteLine("Add 25: ");
-            //innum2 = Console.ReadLine();
-            //innum225 = Convert.ToInt32(innum2) + 25; // convert to integer
-            //Console.WriteLine("Input plus 25 =");
-            //Console.WriteLine(innum225); // display the line
+            string innum2;
+            int innum225;
+            Console.WriteLine("Add 25: ");
+            innum2 = Console.ReadLine();
+            innum225 = Convert.ToInt32(innum2) + 25; // convert to integer
+            Console.WriteLine("Input plus 25 =");
+            Console.WriteLine(innum225); // display the line
 
 
-            //Console.WriteLine("Divide by 12.5: ");
-            //string IN1 = Console.ReadLine(); // gets user input
-            //double numb1 = Convert.ToDouble(IN1);// convert to double
-            //double numb2 = 12.5;
-            //double total = numb1 / numb2; 
-            //Console.WriteLine("Input divided by 12.5 =");
-            //Console.WriteLine(total); // display the line
+            Console.WriteLine("Divide by 12.5: ");
+            string IN1 = Console.ReadLine(); // gets user input
+            double numb1 = Convert.ToDouble(IN1);// convert to double
+            double numb2 = 12.5;
+            double total = numb1 / numb2;
+            Console.WriteLine("Input divided by 12.5 =");
+            Console.WriteLine(total); // display the line
 
-            //Console.WriteLine("Is your input greater than 50?: ");
-            //string IN2 = Console.ReadLine(); // gets user input
-            //int IN3 = Convert.ToInt32(IN2);
-            //bool trueOrFalse = 50 < IN3;
-            //Console.Write(trueOrFalse.ToString());
-            //Console.Read();
+
+            Console.WriteLine("Is your input greater than 50?: ");
+            string IN2 = Console.ReadLine(); // gets user input
+            int IN3 = Convert.ToInt32(IN2);// convert to integer
+            bool trueOrFalse = 50 < IN3;//askeed if user input is greater than 50
+            Console.Write(trueOrFalse.ToString());//then prints 
+            Console.Read();
 
 
             Console.WriteLine("whats the remainder if we divide by 7?: ");
             string IN4 = Console.ReadLine(); // gets user input
-            int IN5 = Convert.ToInt32(IN4);
-            int remander = IN5 % 7;
-            Console.WriteLine(remander);
+            int IN5 = Convert.ToInt32(IN4);// converts the string to a remainder.
+            int remander = IN5 % 7; //Takes an input from the user, divides it by 7
+            Console.WriteLine(remander);//then prints the remainder to the console(tip: think % operator).
             Console.Read();
-            //Takes an input from the user, divides it by 7, then prints the remainder to the console(tip: think % operator).
+            
 
 
 
