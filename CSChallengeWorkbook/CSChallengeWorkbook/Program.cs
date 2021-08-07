4853cdef87cc7e16c3047f4e676943c1eb59d744
Joshua Watson <watson0117@gmail.com>
2021-08-06 19:38:17 -0600

    console app exercise

--- CSChallengeWorkbook/CSChallengeWorkbook/Program.cs
+++ CSChallengeWorkbook/CSChallengeWorkbook/Program.cs
@@ -6,23 +6,76 @@ namespace CSChallengeWorkbook
     {
         static void Main()
         {
-            int num1 = 3;
-            int num2 = 9;
 
-            int addTotal = num1 + num2;
-            Console.WriteLine(addTotal);
 
-            int subTotal = num2 - num1;
-            Console.WriteLine(subTotal);
+           
 
-            int divTotal = num2 / num1;
-            Console.WriteLine(divTotal);
 
-            int mTotal = num1 * num2;
-            Console.WriteLine(mTotal);
+            //string innum1;
+            //int innum150;
+            //Console.WriteLine("multiply by 50: ");
+            //innum1 = Console.ReadLine();
+            //innum150 = Convert.ToInt32(innum1) * 50; // convert to integer
+            //Console.WriteLine("Input multiplied by 50 =");
+            //Console.WriteLine(innum150); // display the line
 
-            int num3 = 200 / 3;
-            Console.WriteLine(num3);
+
+            //string innum2;
+            //int innum225;
+            //Console.WriteLine("Add 25: ");
+            //innum2 = Console.ReadLine();
+            //innum225 = Convert.ToInt32(innum2) + 25; // convert to integer
+            //Console.WriteLine("Input plus 25 =");
+            //Console.WriteLine(innum225); // display the line
+
+
+            //Console.WriteLine("Divide by 12.5: ");
+            //string IN1 = Console.ReadLine(); // gets user input
+            //double numb1 = Convert.ToDouble(IN1);// convert to double
+            //double numb2 = 12.5;
+            //double total = numb1 / numb2; 
+            //Console.WriteLine("Input divided by 12.5 =");
+            //Console.WriteLine(total); // display the line
+
+            //Console.WriteLine("Is your input greater than 50?: ");
+            //string IN2 = Console.ReadLine(); // gets user input
+            //int IN3 = Convert.ToInt32(IN2);
+            //bool trueOrFalse = 50 < IN3;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.Read();
+
+
+            Console.WriteLine("whats the remainder if we divide by 7?: ");
+            string IN4 = Console.ReadLine(); // gets user input
+            int IN5 = Convert.ToInt32(IN4);
+            int remander = IN5 % 7;
+            Console.WriteLine(remander);
+            Console.Read();
+            //Takes an input from the user, divides it by 7, then prints the remainder to the console(tip: think % operator).
+
+
+
+
+
+
+
+            //int num1 = 3;
+            //int num2 = 9;
+
+            //int addTotal = num1 + num2;
+            //Console.WriteLine(addTotal);
+
+            //int subTotal = num2 - num1;
+            //Console.WriteLine(subTotal);
+
+            //int divTotal = num2 / num1;
+            //Console.WriteLine(divTotal);
+
+            //int mTotal = num1 * num2;
+            //Console.WriteLine(mTotal);
+
+            //int num3 = 200 / 3;
+            //Console.WriteLine(num3);
         }
     }
 }
