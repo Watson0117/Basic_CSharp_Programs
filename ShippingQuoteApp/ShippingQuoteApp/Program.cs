cf6dd2aac69a23903cce3adba5c67dda761d7a7e
Joshua Watson <watson0117@gmail.com>
2021-08-25 04:44:09 -0600

    Updated
    removed unnecessary if statements

--- ShippingQuoteApp/ShippingQuoteApp/Program.cs
+++ ShippingQuoteApp/ShippingQuoteApp/Program.cs
@@ -7,18 +7,10 @@ namespace ShippingQuoteApp
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");// This line greats the customer
+
             Console.WriteLine("Please enter your packages weight");// this line writes the question to the console
             string PWTS = Console.ReadLine();
             int PackageWeight = Convert.ToInt32(PWTS);
-
-            if (PackageWeight > 50) // This line makes the console.writeline below appear if the answer is true
-            {
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");// If the package is to heavy it will write thsi line
-            }
-            else
-            {
-                Console.WriteLine("Package within acceptible weight.\n"); // I added this more for me to make sure everything was working correctly.
-            }
 
             Console.WriteLine("Please enter your packages width");// this line write the question to the console
             string PWS = Console.ReadLine();// this line gets user input and store it as a string
@@ -32,15 +24,6 @@ namespace ShippingQuoteApp
             string PHS = Console.ReadLine();// this line gets user input and store it as a string
             int PackageHeight = Convert.ToInt32(PHS);// this line converts it and stores it as an integer
 
-            if (PackageHeight + PackageLength + PackageWidth > 50)//this line makes sure the combine total demensions are less than or equal to 50
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express."); // If the package is to big it will write this line
-            }
-            else
-            {
-                Console.WriteLine("Package within acceptible dimensions.\n");// I added this more for me to make sure everything was working correctly.
-            }
-
             int PackageDimensions = (PackageWidth * PackageLength * PackageHeight);// this line creates Packagedimensions total
             int PackageValue = (PackageDimensions * PackageWeight);// this multplies the packages demension by the Packages weight
             int PackagePrice = (PackageValue / 100);// this take the results of Package value and divides it by 100 to get a price
@@ -49,6 +32,10 @@ namespace ShippingQuoteApp
             {
                 Console.WriteLine("Your estimated total for shipping this package is:");// this line addes some text to make everything look better.
                 Console.WriteLine(PackagePrice.ToString("C"));// this line takes the price and changes it to a dollor amount and writes it to the console.
+            }
+            else
+            {
+                Console.WriteLine("Your package is ether too bi or to heavy to be shipped with express. Have a good day.");
             }
     
         }
