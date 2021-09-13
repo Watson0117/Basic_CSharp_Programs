aa2c1ff4b7bb74fff8076bdc57fc007248cd7328
Joshua Watson <watson0117@gmail.com>
2021-09-13 12:33:30 -0600

    update

--- DoWileApp/DoWileApp/Program.cs
+++ DoWileApp/DoWileApp/Program.cs
@@ -14,13 +14,13 @@ namespace DoWileApp
             {
                 switch (number)
                 {
-                    case 42:
+                    case 42: // this sets the correct answer to 42 anthing else will give the response below
                         Console.WriteLine("you choose correctly");
-                        isGuessed = true;
-                        break;
-                    default:
+                        isGuessed = true;// this set the answer 42 as true makeing the below wile statmnet work
+                        break;// this line stope the program from continuing to the default
+                    default: // this line set the default output when the user dosent guess correctly
                         Console.WriteLine("you choose wrong, but you can guess again");
-                        Console.WriteLine("Guess a number.");
+                        Console.WriteLine("Guess a number.");// this lina write to ask the user to give another guess
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
                 }
@@ -28,13 +28,13 @@ namespace DoWileApp
             while (!isGuessed);
 
             
-            while (isGuessed == true)
+            while (isGuessed == true)// when the answer is 42 this will trigger
             {
-                Console.WriteLine("The answer, is indeed 42. Altho it is meaningless because you never knew what the question was.");
+                Console.WriteLine("The answer, is indeed 42. Although it is meaningless because you never knew what the question was.");
                 break;
             }
                
-            Console.ReadLine();
+            Console.ReadLine();//this keeps the app open
         }
     }
 }
