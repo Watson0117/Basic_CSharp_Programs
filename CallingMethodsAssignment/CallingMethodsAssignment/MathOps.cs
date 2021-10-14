111b4c9714fa6a093dccb28c2f79978874075af2
Joshua Watson <watson0117@gmail.com>
2021-10-14 02:47:28 -0600

    Issues 10/14/21

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/MathOps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethodsAssignment
+{
+    public class MathOps
+    {
+        static void Main(string[] args)
+        {
+            string MOUI = UI.convertToint32();
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CallingMethodsAssignment.MathOps;
+
+
+namespace CallingMethodsAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //MathOps MO = new MathOps.MODIV();
+            Console.WriteLine("Welcome, Please pick a number!");
+            string UI = Console.ReadLine();
+            Console.WriteLine(UI.MODIV);
+            Console.WriteLine(UI.MOMUL);
+            Console.WriteLine(UI.MOADD);
+            Console.WriteLine(UI.MathOps.MODIV);
+        }
+    }
+}
