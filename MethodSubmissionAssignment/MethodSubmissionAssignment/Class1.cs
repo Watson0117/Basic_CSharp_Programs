f9a6cec507e783fe93ab2c32581e8778946cab76
Joshua Watson <watson0117@gmail.com>
2021-10-23 23:58:16 -0600

    10/23/21 Update
    need help on the assignmnet

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodSubmissionAssignment
+{
+    public class Class1
+    {
+
+
+        public static int Method1(int Num1)  // takes in an int and returns and int
+        {
+            int Answer1 = Num1 + 10;
+            return Answer1;
+        }
+
+
+        public static int Method1(int Num1, int Num2)  // takes in an int and returns and int
+        {
+            int Answer2 = Num1 + Num2;
+            return Answer2;
+        }
+
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick 2 numbers the first is requiered and the secod optional");
+            string UserInput1 = Console.ReadLine();
+            string UserInput2 = Console.ReadLine();
+            int Num1 = Convert.ToInt32(UserInput1);
+            int Num2 = Convert.ToInt32(UserInput2);
+
+            Class1 Class1 = new Class1();
+
+
+            
+            if (Num2 < 1) 
+            {
+                int OneEntry = Class1.Method1(Num1);// this line calls the method 
+                Console.WriteLine(OneEntry);
+            }
+            else
+            {
+                int TwoEntry = Class1.Method1(Num1, Num2);// this line calls the method 
+                Console.WriteLine(TwoEntry);
+            }
+
+            
+
+        }
+
+        
+    }
+}
