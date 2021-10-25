54645e4bc43e2ec30ecd99938f1c86c8c0f47119
Joshua Watson <watson0117@gmail.com>
2021-10-25 13:16:07 -0600

    10/25/21 1:13pm update.
    i do believe i have the Method Submission Assignment completed.

--- MethodSubmissionAssignment/MethodSubmissionAssignment/Class1.cs
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Class1.cs
@@ -7,14 +7,6 @@ namespace MethodSubmissionAssignment
     public class Class1
     {
 
-
-        public int Method1(int Num1)  // takes in an int and returns and int
-        {
-            int Answer1 = Num1 + 10;
-            return Answer1;
-        }
-
-
         public int Method1(int Num1, int Num2)  // takes in 2 int and returns and int
         {
             int Answer2 = Num1 + Num2;
--- MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -8,19 +8,34 @@ namespace MethodSubmissionAssignment
         {
             Console.WriteLine("Please pick 2 numbers the first is requiered and the secod optional");
             string UserInput1 = Console.ReadLine();
-            string UserInput2 = Console.ReadLine();
             int Num1 = Convert.ToInt32(UserInput1);
-            int Num2 = Convert.ToInt32(UserInput2);
+
+            string UserInput2 = Console.ReadLine();
+            
 
             Class1 Class1 = new Class1();
+            try
+            {
+                int Num2 = Convert.ToInt32(UserInput2);
+                int TwoEntry = Class1.Method1(Num1, Num2);// this line calls the method 
+                Console.WriteLine(TwoEntry);
+            }
+            
+            catch
+            {
+               
+                int Num2 = 10;
+                int TwoEntry = Class1.Method1(Num1, Num2);// this line calls the method 
+                Console.WriteLine(TwoEntry);
+                
+            }
 
+            
 
-            int OneEntry = Class1.Method1(Num1);// this line calls the method 
-            Console.WriteLine(OneEntry);
+            
+            
 
-
-            int TwoEntry = Class1.Method1(Num1, Num2);// this line calls the method 
-            Console.WriteLine(TwoEntry);
+            
 
         }
 
