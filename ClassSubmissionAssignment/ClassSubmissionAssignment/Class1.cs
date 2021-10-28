a88907a8abb1ee3f17e8e192255dd5de192dda8c
Joshua Watson <watson0117@gmail.com>
2021-10-28 17:24:13 -0600

    10/28/21 5:23pm ClassSubmissionAssignment

---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassSubmissionAssignment
+{
+    public class Class1
+    {
+        public void Method1(int Number1)  // takes in int and outputs an int
+        {
+ 
+        // Declaring variable
+        // without assigning value
+        int Num1 = Number1;
+ 
+        // Pass variable i to the method
+        // using out keyword
+        Division(out Num1);
+ 
+        // Display the value i
+        Console.WriteLine("The Division of the value is: {0}", Num1);
+        }
+ 
+        // Method in which out parameter is passed
+        // and this method returns the value of
+        // the passed parameter
+        public static void Division(out int Number1)
+        {
+            int Num1;
+            Number1 = Num1 / 2;
+            Console.WriteLine(Number1);
+        }
+
+    }
+}
---
+++ ClassSubmissionAssignment/ClassSubmissionAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick a number that will be divided by 2.");
+            string UserInput1 = Console.ReadLine();
+            
+            int Number1 = Convert.ToInt32(UserInput1);
+            
+
+            Class1 Class1 = new Class1();
+
+            try
+            {
+                Class1.Method1(Number1);// this line calls the method   
+            }
+            catch
+            {
+                Console.WriteLine("There seems to be an error");
+            }
+        }
+    }
+}
