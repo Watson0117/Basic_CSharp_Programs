5fac317e8abb20f82b578df826578944ea610535
Joshua Watson <watson0117@gmail.com>
2021-10-27 23:28:49 -0600

    10/27/21 11:28pm Update

--- MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Class1.cs
+++ MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Class1.cs
@@ -6,11 +6,10 @@ namespace MethodClassSubmissionAssignment
 {
     public class Class1
     {
-
-        public int Method1(int Num1)  // takes in 2 int and returns and int
+        public int Method1(int Num1, int Num2)  // takes in 2 int and returns and int
         {
-            int Answer2 = Num1 + 10;
-            return Answer2;
+            int Answer2 = Num1 + 100;
+            return Num2;
         }
 
     }
--- MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Program.cs
+++ MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Program.cs
@@ -8,20 +8,17 @@ namespace MethodClassSubmissionAssignment
         {
             Console.WriteLine("Please pick 2 numbers the first is will have 10 added to it and then it will display the second below that");
             string UserInput1 = Console.ReadLine();
-            int Num1 = Convert.ToInt32(UserInput1);
-
             string UserInput2 = Console.ReadLine();
-
+            int Num1 = Convert.ToInt32(UserInput1);
+            int Num2 = Convert.ToInt32(UserInput2);
 
             Class1 Class1 = new Class1();
-            try
-            {
-                int Num2 = Convert.ToInt32(UserInput2);
-                int TwoEntry = Class1.Method1(Num1);// this line calls the method 
-                Console.WriteLine(TwoEntry);
-                Console.WriteLine(Num2);
-            }
 
+            try
+            {   
+                int TwoEntry = Class1.Method1(Num1, Num2);// this line calls the method 
+                Console.WriteLine(TwoEntry);
+            }
             catch
             {
                 Console.WriteLine("There seems to be an error");
