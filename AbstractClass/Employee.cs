4c745baf26401fa7352c366f6d853e0233217b4d
Joshua Watson <watson0117@gmail.com>
2021-11-15 15:20:58 -0700

    Abstract class submission assignment 1st commit 11/15/21
    First commit of Abstract Class assignment

---
+++ AbstractClass/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass//Create another class called Employee and have it inherit from the Person class.
+
+//Implement the SayName() method inside of the Employee class.
+{
+    class Employee: Person
+    {
+        public override void SayName()
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: " + FullName);
+        }
+    }
+}
---
+++ AbstractClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Employee Nemp = new Employee();//Inside the Main() method, instantiate an Employee object
+            {
+                Nemp.FirstName = "Sample";//with firstName “Sample”
+                Nemp.LastName = "Student";// and lastName “Student”. 
+                Nemp.SayName();//Call the SayName() method on the object.
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
