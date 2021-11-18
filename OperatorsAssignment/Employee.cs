d6da10d5772a6bc791ebb6c195275439edf5bed1
Joshua Watson <watson0117@gmail.com>
2021-11-17 17:23:27 -0700

    OperatorAssignment update 11/17/21

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+
+        public override void SayName()
+        {
+            string FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: " + FullName);
+
+        }
+
+        public static Employee operator ==(Employee EmployeeId, Person person)
+        {
+            EmployeeId.Equals(EmployeeId);
+            Console.WriteLine("Employee 0117 Active");
+        }
+        public static Employee operator !=(Employee EmployeeId, Person person)
+        {
+            EmployeeId !=  EmployeeId;
+            Console.WriteLine("Employee 0117 NOT Active");
+        }
+
+    }
+}
---
+++ OperatorsAssignment/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void SayName();//Give it the method SayName(). This should take the first and last names and concatinate them into one string called FullName
+    }
+}
