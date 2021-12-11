1f95eaabe87b01d6dfaa7fb53c0acce4d7728b3a
Joshua Watson <watson0117@gmail.com>
2021-12-11 04:35:36 -0700

    projects updates base

---
+++ InputAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Perform these actions and create a console app that includes the following:
+            //Asks the user for a number.
+            //Logs that number to a text file.
+            //Prints the text file back to the user.
+            //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+            //Upload your code to GitHub.
+            //Note: The code in this assignment does not need to be reviewed by an Instructor, so you will not be submitting a link to GitHub.
+        }
+    }
+}
