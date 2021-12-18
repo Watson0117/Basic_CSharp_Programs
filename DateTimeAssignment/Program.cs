bbb170a9d52b5ef8a3450545363cad238b2d5040
Joshua Watson <watson0117@gmail.com>
2021-12-18 01:02:15 -0700

    DateTime Assignment - done - Updated@ 12/18/21 - 1:01AM

--- DateTimeAssignment/Program.cs
+++ DateTimeAssignment/Program.cs
@@ -7,14 +7,16 @@ namespace DateTimeAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-
-                //Perform these actions and create a console app that includes the following:
-                //Prints the current date and time to the console.
-                //Asks the user for a number.
-                //Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
-                //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
-                //Upload your code to GitHub and submit the link below:
+            DateTime dateTimeNow = DateTime.Now;
+            Console.WriteLine(dateTimeNow);//Prints the current date and time to the console.
+            Console.WriteLine("Press any key to continue.");//to keep track of program position. writes to console so i can see it ready to go to next step
+            Console.ReadLine();//Keeps console open without activating next line
+            Console.WriteLine("Please pick a number of Hours"); //Asks the user for a number.
+            int Hours = Convert.ToInt32(Console.ReadLine());// takes the number enterd by the use and converts it to an int32
+            DateTime dateTimeThen = dateTimeNow.AddHours(Hours);// creates a new datetime obj that is the current date with i days added.
+            Console.WriteLine(dateTimeThen);//Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+            Console.WriteLine("Press any key to continue.");//to keep track of program position. writes to console so i can see it ready to go to next step
+            Console.ReadLine();//Keeps console open without activating next line
         }
     }
 }
