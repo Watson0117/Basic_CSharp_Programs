6a1a75ff45de6574cdc198756a474081cb4befcd
Joshua Watson <watson0117@gmail.com>
2021-12-19 05:10:07 -0700

    ConstructorAssignment update 12/19/21

--- ConstructorAssignment/Program.cs
+++ ConstructorAssignment/Program.cs
@@ -7,17 +7,9 @@ namespace ConstructorAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-           
-            //Create a const variable.
-
-            //Create a variable using the keyword “var.”
-
-            //Chain two constructors together.
-
-            //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
-
-            //Upload your code to GitHub and submit the link below:
-
+            const string Words = "Words here, ";//Create a const variable.
+            var moreWords = "and more words here.";//Create a variable using the keyword “var.”
+            Console.WriteLine(Words + moreWords);//Chain two constructors together.
         }
     }
 }
