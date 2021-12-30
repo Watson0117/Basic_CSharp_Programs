0ed3926aab36d3715c623075e781acdbf873bcf6
Joshua Watson <watson0117@gmail.com>
2021-12-30 00:05:21 -0700

    updated

--- ConstructorAssignment/Program.cs
+++ ConstructorAssignment/Program.cs
@@ -6,10 +6,22 @@ namespace ConstructorAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            const string Words = "Words here, ";//Create a const variable.
-            var moreWords = "and more words here.";//Create a variable using the keyword “var.”
-            Console.WriteLine(Words + moreWords);//Chain two constructors together.
+            myConstClass obj = new myConstClass();
+        }
+        public class myConstClass
+        {
+            public myConstClass() : this(10)
+            {
+                var moreWords = "1347 is 1337 plus 10";//Create a variable using the keyword “var.”// No parameter constructor method.// First Constructor  
+                Console.WriteLine(moreWords);//Chain two constructors together.
+            }
+            public myConstClass(int num)
+            {
+                int num2 = num;
+                const int leet = 1337;//Create a const variable.
+                int leetPlusNum = leet + num;
+                Console.WriteLine(leetPlusNum);//Chain two constructors together.// Constructor with one parameter.// Second Constructor}  
+            }
         }
     }
 }
