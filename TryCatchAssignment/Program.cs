86d29b665c2b8fdccf2b28fbbda6060bf4f82496
Joshua Watson <watson0117@gmail.com>
2021-12-29 19:08:24 -0700

    try/catch update

--- TryCatchAssignment/Program.cs
+++ TryCatchAssignment/Program.cs
@@ -6,25 +6,28 @@ namespace TryCatchAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             Console.WriteLine("How old are you?");//Ask the user for their age.
             int age = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine(DateTime.Now - age);//Display the year the user was born.
             try
-            {
-
+            { 
+                DateTime now = DateTime.Now;
+                int yearN = now.Year;
+                int dOb = yearN - age;
+                DateTime yearOfBirth = new DateTime(dOb, 1, 13, 3, 57, 32, 11);
+                int yearOB = yearOfBirth.Year;
+                Console.WriteLine(yearOB);//Display the year the user was born.
             }
-            catch
+            catch (Exception ex)//Exceptions must be handled using “try/catch.”
             {
-
+                if (age <= 0)//Display appropriate error messages if the user enters zero or negative numbers.
+                {
+                    Console.WriteLine("You must enter a number above 0");
+                }
+                else //Display a general message if an exception was caused by anything else.
+                {
+                    Console.WriteLine(ex.Message); 
+                }
             }
-        //Exceptions must be handled using “try/catch.”
-        //Display appropriate error messages if the user enters zero or negative numbers.
-        //Display a general message if an exception was caused by anything else.
-        //Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
-        //Upload your code to GitHub.
         }
     }
 }
