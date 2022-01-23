6a9210a531bebf0d87434ada86759b2bd6c2d457
Joshua Watson <watson0117@gmail.com>
2022-01-23 03:00:31 -0700

    FCSA initial commit

---
+++ FinalChallangeSubmissionAssignment/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Linq;
+using System.Data.Entity;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FinalChallangeSubmissionAssignment
+{
+
+}
+public class Program
+{
+   
+
+    public static void Main()
+	{
+		using (var ctx = new SchoolContext())
+		{
+			var stud = new Student() { StudentName = "Bill" };
+			
+
+			ctx.Students.Add(stud);
+			ctx.SaveChanges();
+
+			Console.Write("Student saved successfully!");
+			Console.Write("\n" + stud.StudentName + "\n" + stud.StudentID + "\n" + stud.DateOfBirth);
+			Console.ReadLine();
+
+
+		}
+	}
+}
+
+public class SchoolContext : DbContext
+{
+	public SchoolContext() : base()
+	{
+	}
+
+	public DbSet<Student> Students { get; set; }
+	public DbSet<Grade> Grades { get; set; }
+}
+
+public class Student
+{
+	public int StudentID { get; set; }
+	public string StudentName { get; set; }
+	public DateTime? DateOfBirth { get; set; }
+	public byte[] Photo { get; set; }
+	public decimal Height { get; set; }
+	public float Weight { get; set; }
+
+	public Grade Grade { get; set; }
+}
+
+public class Grade
+{
+	public int GradeId { get; set; }
+	public string GradeName { get; set; }
+	public string Section { get; set; }
+
+	public ICollection<Student> Students { get; set; }
+}
+
+
+
+
